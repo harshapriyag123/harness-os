e78e8da5e4bfc12f10611a112675ba11006dcb1e
Harsha Priya Ganapathy <121929535+harshapriyag123@users.noreply.github.com>
2026-09-02 13:25:30 -0500

    feat: mount full evidence demo in operator build

--- frontend/src/mission-entry.tsx
+++ frontend/src/mission-entry.tsx
@@ -1,6 +1,7 @@
 import React from'react';
 import{createRoot}from'react-dom/client';
 import MissionControl from'./MissionControl';
+import DemoEnhancements from'./DemoEnhancements';
 import JudgeDemoCore from'./JudgeDemoCore';
 import PublicMissionControl from'./PublicMissionControl';
 
@@ -9,6 +10,6 @@ const publicReadOnly=import.meta.env.VITE_PUBLIC_READ_ONLY==='true';
 
 if(root)createRoot(root).render(
  <React.StrictMode>
-  {publicReadOnly?<PublicMissionControl/>:<><MissionControl/><JudgeDemoCore/></>}
+  {publicReadOnly?<PublicMissionControl/>:<><MissionControl/><DemoEnhancements/><JudgeDemoCore/></>}
  </React.StrictMode>
 );
